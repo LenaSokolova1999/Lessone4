8c07283d8edd04aba9cf517384798a0c34ed36ef
Lena Sokolova <sokolovalena1999@mail.ru>
2021-07-23 11:33:26 +0300

    first commit

---
+++ Lessone4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lessone4
+//
+//  Created by Mac Mini 2018 on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
